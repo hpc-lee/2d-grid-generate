7480a593fd59271e8e7a45754a30e14ab5c50be4
李华林 <12131168@mail.sustech.edu.cn>
2024-08-07 16:46:18 +0800

    modify ellptic makefile

--- elliptic/run_make.sh
+++ elliptic/run_make.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#export MPIHOME=/data3/lihl/software/openmpi-gnu-4.1.2
+#export NETCDF=/data/apps/NetCDF/disable-netcdf-4.8.1
+
 export MPIHOME=/data/apps/openmpi/4.1.5-cuda-aware
 export NETCDF=/data/apps/NetCDF/disable-netcdf-4.8.1
 
